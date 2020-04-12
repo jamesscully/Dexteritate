ad6b13d1d5501f2da6bbb975551762a247cf1bfd
James Scully <james.will.scully@gmail.com>
2020-04-12 21:11:33 +0100

    First commit

---
+++ game/Dexteritate/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private float vertV = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        float leftright = Input.GetAxis("Mouse X");
+        float updown = Input.GetAxis("Mouse Y");
+        float forwards = Input.GetAxis("Vertical");
+        float sideways = Input.GetAxis("Horizontal");
+        
+        transform.Rotate(0, leftright, 0);
+        Camera.main.transform.Rotate(updown, 0, 0);
+
+        vertV += Physics.gravity.y * Time.deltaTime;
+        
+        CharacterController 
+
+
+
+
+    }
+}
